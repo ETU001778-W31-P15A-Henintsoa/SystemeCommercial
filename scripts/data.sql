b7ef42646a7868aae55388640732e95c90bc50d7
Henintsoa <118676248+ETU001778-W31-P15A-Henintsoa@users.noreply.github.com>
2023-11-18 20:06:45 +0300

    commit Base
    Base de donnees mail fournisseur ville message, data ville et fournisseur

---
+++ scripts/data.sql
@@ -0,0 +1,11 @@
+------------------------------------ VILLE ---------------------------------------
+insert into Ville(nomVille) values
+ ('Antananarivo'),
+ ('Antsirable');
+
+------------------------------------ FOURNISSEUR ---------------------------------
+insert into Fournisseur(nomFournisseur, email, motdepasse, adresse, idVille) values 
+('SHOPRITE', 'shoprite@gmail.com', 'shoprite', 'Lot II BIS Tanjobato', 'VILLE1'),
+('JUMBO SCORE', 'jumbo@gmail.com', 'jumbo', 'Lot III BIS Ankorondrano', 'VILLE1'),
+('LEADER PRICE', 'lederprice@gmail.com', 'leaderprice', 'Lot VI BIS Ankorondrano', 'VILLE1');
+
--- scripts/database.sql
+++ scripts/database.sql
@@ -1,3 +1,47 @@
 -- creation de la base de donnees
 create database systemecommercial;
 \c systemecommercial
+
+------------------------------------ VILLE ---------------------------------------
+create sequence seqVille;
+create table Ville(
+    idVille varchar(20) default concat('VILLE' || nextval('seqVille')) primary key,
+    nomVille varchar(30) -- Nom de la ville
+);
+
+------------------------------------ ENTREPRISE ---------------------------------------
+create sequence seqEntreprise;
+create table Entreprise(
+    idEntreprise varchar(20) default concat('ENT' || nextval('seqEntreprise')) primary key,
+);
+
+------------------------------------ FOURNISSEUR ---------------------------------
+create sequence seqFournisseur;
+create table Fournisseur(
+    idFournisseur varchar(20) default concat('FOU' || nextval('seqFournisseur')) primary key,
+    nomFournisseur varchar(30), -- Nom de la societe
+    email varchar(20),
+    motdepasse varchar(20),
+    adresse varchar(50),
+    idVille varchar(20),
+    foreign key (idVille) references Ville(idVille)
+);
+
+------------------------------------ EMAIL ----------------------------------------
+create sequence seqMail;
+create table Mail(
+    idMail varchar(20) default concat('MAIL' || nextval('seqMail')) primary key,
+    dateenvoie timestamp,
+    envoyeur varchar(20),
+    destinataire varchar(20)
+);
+
+------------------------------------ MESSAGE --------------------------------------
+create sequence seqMessage;
+create table Message(
+    idMessage varchar(20) default concat('MESS' || nextval('seqMessage')) primary key,
+    idMail varchar(20),
+    libelle text,
+    piecejointe varchar(20),
+    foreign key (idMail) references Mail(idMail)
+);
