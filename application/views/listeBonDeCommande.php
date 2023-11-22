ac76b346a66d5c3ed4ec43a1a066277b79c539b9
SantatraM <santatryn@gmail.com>
2023-11-22 10:54:35 +0300

    commit

--- application/views/listeBonDeCommande.php
+++ application/views/listeBonDeCommande.php
@@ -6,9 +6,40 @@
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
               <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Bon de </span> Commande</h4>
+
+
+              <div class="card">
+                    <h5 class="card-header">Bon de Commande  Valides par DG</h5>
+                    <div class="text-nowrap">
+                    <table class="table">
+                        <!-- <thead> -->
+                        <tr>
+                        <th><strong>idBonDeCommande</strong></th>
+                            <th><strong>Fournisseur</strong></th>
+                            <th><strong>date bon de commande</strong></th>
+                            <th><strong></strong></th>
+                            <th><strong></strong></th>
+                        </tr>
+                        <!-- </thead> -->
+                        <tbody class="table-border-bottom-0">
+                        <?php foreach($bonDeCommandeValideDG as $bdcvdg) { ?>
+                        <tr>
+                            <td><?php echo $bdcvdg['idbondecommande'];  ?></td>
+                            <td><?php echo $bdcvdg['idfournisseur']; ?></td>
+                            <td><?php echo $bdcvdg['datebondecommande']; ?></td>
+                            <td><span class="badge bg-label-warning me-1">Non Valide</span></td>
+                            <td>
+                                <a href="<?php echo site_url("BonDeCommande/versDetailBonDeCommande?id=".$bdcvdg['idbondecommande']); ?>">Voir Detail</a>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
         
               <div class="card">
-                    <h5 class="card-header">Bon de Commande  Valides</h5>
+                    <h5 class="card-header">Bon de Commande  Valides par finance</h5>
                     <div class="text-nowrap">
                     <table class="table">
                         <!-- <thead> -->
